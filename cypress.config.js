c1094be3a6c3d4f43553354bc04d4069ebc39c3c
stefu <sotajagic@gmail.com>
2022-07-18 13:44:35 +0300

    e2e pageLoadTimeout to 200000

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    pageLoadTimeout: 200000,
     // eslint-disable-next-line no-unused-vars
     setupNodeEvents(on, config) {
       // implement node event listeners here
