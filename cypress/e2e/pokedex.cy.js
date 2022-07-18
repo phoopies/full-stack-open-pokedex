5bf140490e0d4c381496da1f6991bd64a5d7859b
stefu <sotajagic@gmail.com>
2022-07-18 13:30:49 +0300

    Changed test:e2e script to use chrome browser

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,5 +1,7 @@
 describe('Pokedex', function() {
   it('front page can be opened', function() {
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(1000)
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
